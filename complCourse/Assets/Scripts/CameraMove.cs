dad82e04b903f69a585d332d0fee26edf240111c
Miroslav Miroslavov <martinsen06@gmail.com>
2024-03-02 09:02:06 +0200

    5 Rotate camera by speed

--- complCourse/Assets/Scripts/CameraMove.cs
+++ complCourse/Assets/Scripts/CameraMove.cs
@@ -5,8 +5,32 @@ using UnityEngine;
 public class CameraMove : MonoBehaviour
 {
   public Transform playerTransform;
+  public Rigidbody playerRigidbody;
+
+  public List<Vector3> VelocitiesList = new List<Vector3>();
+
+  private void Start() {
+    for (int i = 0; i < 10; i++)
+    {
+      VelocitiesList.Add(Vector3.zero);
+    }
+  }
+
+  private void FixedUpdate() {
+    VelocitiesList.Add(playerRigidbody.velocity);
+    VelocitiesList.RemoveAt(0);
+  }
+
+
   void Update()
   {
+    Vector3 summ = Vector3.zero;
+
+    for (int i = 0; i < VelocitiesList.Count; i++)
+    {
+      summ += VelocitiesList[i];
+    }
     transform.position = playerTransform.position;
+    transform.rotation =  Quaternion.Lerp( transform.rotation, Quaternion.LookRotation(summ), Time.deltaTime * 10f);
   }
 }
