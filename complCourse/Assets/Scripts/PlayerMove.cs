1f49589ab1dc1c826ba26444839fb7a973c18efb
Miroslav Miroslavov <martinsen06@gmail.com>
2024-03-27 08:01:37 +0200

    2. jump,Collision

--- complCourse/Assets/Scripts/PlayerMove.cs
+++ complCourse/Assets/Scripts/PlayerMove.cs
@@ -2,19 +2,52 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float MoveSpeed;
-    public float JumpSpeed;
-    public float Friction;
-    public bool Grounded;
-    private Rigidbody rb;
+  public float MoveSpeed;
+  public float JumpSpeed;
+  public float Friction;
+  public bool Grounded;
+  private Rigidbody rb;
 
-    private void Start() {
-      rb = GetComponent<Rigidbody>();
+  private void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+  }
+
+  private void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.Space))
+    {
+      if (Grounded)
+      {
+        rb.AddForce(0, JumpSpeed, 0, ForceMode.VelocityChange);
+      }
+    }
+  }
+
+  private void FixedUpdate()
+  {
+    rb.AddForce(Input.GetAxis("Horizontal") * MoveSpeed, 0, 0, ForceMode.VelocityChange);
+    rb.AddForce(-rb.velocity.x * Friction, 0, 0, ForceMode.VelocityChange);
+  }
+
+  private void OnCollisionStay(Collision other)
+  {
+
+    for (int i = 0; i < other.contactCount; i++)
+    {
+      float angle = Vector3.Angle(other.contacts[i].normal, Vector3.up);
+      if (angle < 45f)
+      {
+        Grounded = true;
+      }
     }
 
-    private void FixedUpdate() {
-      rb.AddForce(Input.GetAxis("Horizontal") * MoveSpeed, 0, 0, ForceMode.VelocityChange);
-      rb.AddForce(-rb.velocity.x * Friction, 0, 0, ForceMode.VelocityChange);
-    }
+    
+  }
+
+  private void OnCollisionExit(Collision other)
+  {
+    Grounded = false;
+  }
 
 }
