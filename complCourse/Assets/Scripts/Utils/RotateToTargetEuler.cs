24c1a7f7859ed214780f5f53cbe422436a1645bd
Miroslav Miroslavov <martinsen06@gmail.com>
2024-03-30 19:32:00 +0200

    28. Pig Movement

---
+++ complCourse/Assets/Scripts/Utils/RotateToTargetEuler.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class RotateToTargetEuler : MonoBehaviour
+{
+
+  public Vector3 LeftEuler;
+  public Vector3 RightEuler;
+  public float RotationSpeed;
+
+  private Vector3 _targetEuler;
+
+  private void Update() {
+    transform.localRotation  = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(_targetEuler), Time.deltaTime * RotationSpeed);
+  }
+
+
+  public void RotateLeft(){
+    _targetEuler = LeftEuler;
+  }
+  public void RotateRight(){
+    _targetEuler = RightEuler;
+  }
+
+}
