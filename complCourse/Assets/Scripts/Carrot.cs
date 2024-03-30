b0d175f2d546591c033c60b8a3cf8fcedc29320e
Miroslav Miroslavov <martinsen06@gmail.com>
2024-03-30 16:08:23 +0200

    24. Rocket from bear mouth

--- complCourse/Assets/Scripts/Carrot.cs
+++ complCourse/Assets/Scripts/Carrot.cs
@@ -6,6 +6,7 @@ public class Carrot : MonoBehaviour
   public float Speed;
 
   private void Start() {
+    transform.rotation = Quaternion.identity;
     Transform playerTransform = FindObjectOfType<PlayerMove>().transform;
     Vector3 toPlayer = (playerTransform.position - transform.position).normalized;
     Rigidbody.velocity = toPlayer * Speed;
---
+++ complCourse/Assets/Scripts/PrefabCreator.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class PrefabCreator : MonoBehaviour
+{
+   public GameObject Prefab;
+   public Transform Spawn;
+
+   public void Create(){
+    Instantiate(Prefab, Spawn.position, Spawn.rotation);
+   } 
+}
