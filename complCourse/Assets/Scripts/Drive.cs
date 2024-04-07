a0c12a07b01423c931b11664f83ed9733cdbe9e2
Miroslav Miroslavov <martinsen06@gmail.com>
2024-04-07 09:08:46 +0300

    3 wheel Physics 3

--- complCourse/Assets/Scripts/Drive.cs
+++ complCourse/Assets/Scripts/Drive.cs
@@ -4,26 +4,31 @@ public class Drive : MonoBehaviour
 {
   public WheelCollider WC;
   public float torque = 200;
+  public float maxSteerAngle = 30;
+  public GameObject Wheel;
     void Start()
     {
         WC = GetComponent<WheelCollider>();
     }
 
-    private void Go(float accel){
+    private void Go(float accel, float steer){
       accel = Mathf.Clamp(accel, -1, 1);
+      steer = Mathf.Clamp(steer, -1, 1) * maxSteerAngle;
       float thrustTorque = torque * accel; 
       WC.motorTorque = thrustTorque;
+      WC.steerAngle = steer;
 
       Quaternion quat;
       Vector3 position;
       WC.GetWorldPose(out position, out quat);
-      transform.position = position;
-      transform.rotation = quat;
+      Wheel.transform.position = position;
+      Wheel.transform.localRotation = quat;
     }
 
     void Update()
     {
         float a  = Input.GetAxis("Vertical");
-        Go(a);
+        float s  = Input.GetAxis("Horizontal");
+        Go(a, s);
     }
 }
