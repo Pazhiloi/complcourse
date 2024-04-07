720456a765d586c7372901e4e5f5cee08ba2c043
Miroslav Miroslavov <martinsen06@gmail.com>
2024-04-07 12:10:02 +0300

    4 Wheel Physics 4

--- complCourse/Assets/Scripts/Drive.cs
+++ complCourse/Assets/Scripts/Drive.cs
@@ -2,33 +2,39 @@ using UnityEngine;
 
 public class Drive : MonoBehaviour
 {
-  public WheelCollider WC;
+  public WheelCollider[] WCs;
+  public GameObject[] Wheels;
   public float torque = 200;
   public float maxSteerAngle = 30;
-  public GameObject Wheel;
-    void Start()
-    {
-        WC = GetComponent<WheelCollider>();
-    }
 
-    private void Go(float accel, float steer){
-      accel = Mathf.Clamp(accel, -1, 1);
-      steer = Mathf.Clamp(steer, -1, 1) * maxSteerAngle;
-      float thrustTorque = torque * accel; 
-      WC.motorTorque = thrustTorque;
-      WC.steerAngle = steer;
+
+  private void Go(float accel, float steer)
+  {
+    accel = Mathf.Clamp(accel, -1, 1);
+    steer = Mathf.Clamp(steer, -1, 1) * maxSteerAngle;
+    float thrustTorque = torque * accel;
+
+    for (int i = 0; i < 4; i++)
+    {
+      WCs[i].motorTorque = thrustTorque;
+      if (i < 2)
+      {
+        WCs[i].steerAngle = steer;
+      }
 
       Quaternion quat;
       Vector3 position;
-      WC.GetWorldPose(out position, out quat);
-      Wheel.transform.position = position;
-      Wheel.transform.localRotation = quat;
+      WCs[i].GetWorldPose(out position, out quat);
+      Wheels[i].transform.position = position;
+      Wheels[i].transform.localRotation = quat;
     }
 
-    void Update()
-    {
-        float a  = Input.GetAxis("Vertical");
-        float s  = Input.GetAxis("Horizontal");
-        Go(a, s);
-    }
+  }
+
+  void Update()
+  {
+    float a = Input.GetAxis("Vertical");
+    float s = Input.GetAxis("Horizontal");
+    Go(a, s);
+  }
 }
