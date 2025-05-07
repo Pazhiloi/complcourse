96f28712b163862bb53c7a14b2566b65d8b8eea1
Miroslav Miroslavov <martinsen06@gmail.com>
2025-05-07 17:18:25 +0300

    40-44 Start create Enemy AI

--- complCourse/Assets/Scripts/Combat/Fighter.cs
+++ complCourse/Assets/Scripts/Combat/Fighter.cs
@@ -41,21 +41,39 @@ namespace RPG.Combat
 
     private void AttackBehaviour()
     {
+      transform.LookAt(target.transform);
       if (timeSinceLastAttack > timeBetweenAttacks)
       {
-        animator.SetTrigger("attack");
+        TriggerAttack();
         timeSinceLastAttack = 0;
-        
+
       }
     }
+
+    private void TriggerAttack()
+    {
+      animator.ResetTrigger("stopAttack");
+      animator.SetTrigger("attack");
+    }
+   
+
     // Animation Event
     void Hit()
     {
+      if (target == null) return;
       target.TakeDamage(weaponDamage);
     }
     private bool GetIsInRange()
     {
       return Vector3.Distance(transform.position, target.transform.position) < weaponRange;
+    }
+
+    public bool CanAttack(CombatTarget combatTarget)
+    {
+      if (combatTarget == null) return false;
+
+      Health targetToTest = combatTarget.GetComponent<Health>();
+      return targetToTest != null && !targetToTest.IsDead();
     }
 
     public void Attack(CombatTarget combatTarget)
@@ -66,10 +84,14 @@ namespace RPG.Combat
 
     public void Cancel()
     {
-      animator.SetTrigger("stopAttack");
+      StopAttack();
       target = null;
     }
 
-    
+    private void StopAttack()
+    {
+      animator.ResetTrigger("attack");
+      animator.SetTrigger("stopAttack");
+    }
   }
 }
--- complCourse/Assets/Scripts/Control/PlayerController.cs
+++ complCourse/Assets/Scripts/Control/PlayerController.cs
@@ -31,7 +31,10 @@ namespace RPG.Control
       {
         CombatTarget target = hit.transform.GetComponent<CombatTarget>();
 
-        if (target == null) continue;
+        if (!fighter.CanAttack(target))
+        {
+          continue;
+        }
 
         if (Input.GetMouseButtonDown(0))
         {
